6b0d37eabe106f8b0c4bda186f5d80e2a4ad666f
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2023-10-15 23:29:30 +0530

    Add files via upload

---
+++ SCRIPT/index.js
@@ -0,0 +1,88 @@
+window.addEventListener("load", function () {
+  this.document.getElementById("preloader").style.display = "none";
+  this.document.getElementById("PL").style.display = "none";
+});
+
+function showPopup() {
+  TP.style.display = "none";
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+  document.body.style.cursor = "pointer";
+  setTimeout(function () {
+    document.getElementById("popup").classList.add("popup-show");
+  }, 100);
+}
+
+function closePopup() {
+  document.getElementById("popup").classList.remove("popup-show");
+  setTimeout(function () {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("popup").style.display = "none";
+    TP.style.display = "block";
+    document.body.style.cursor = "default";
+  }, 500);
+}
+
+setTimeout(function () {
+  showPopup();
+}, 2500);
+
+function myEventHandler() {
+  closePopup();
+}
+
+document.addEventListener("click", myEventHandler, { once: true });
+
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+let TP = document.getElementById("TP");
+
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+  TP.style.display = TP.style.display === "none" ? "block" : "none";
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+const parallax_el = document.querySelectorAll(".Parallax");
+let xValue = 0,
+  yValue = 0;
+let rotateDegree = 0;
+
+function update(cursorPosition) {
+  parallax_el.forEach((el) => {
+    let speedx = el.dataset.speedx;
+    let speedy = el.dataset.speedy;
+    let speedz = el.dataset.speedz;
+    let rotateSpeed = el.dataset.rotation;
+
+    let isInLeft =
+      parseFloat(getComputedStyle(el).left) < window.innerWidth / 2 ? 1 : -1;
+    let zValue =
+      cursorPosition - parseFloat(getComputedStyle(el).left) * isInLeft * 0.1;
+
+    el.style.transform = ` rotateY(${
+      rotateDegree * rotateSpeed
+    }deg) translateX(calc(-50% + ${
+      -xValue * speedx
+    }px)) translateY(calc(-50% + ${
+      yValue * speedy
+    }px)) perspective(2300px) translateZ(${zValue * speedz}px)`;
+  });
+}
+
+update(0);
+
+window.addEventListener("mousemove", (e) => {
+  xValue = e.clientX - window.innerWidth / 2;
+  yValue = e.clientY - window.innerHeight / 2;
+  rotateDegree = (xValue / (window.innerWidth / 2)) * 20;
+
+  update(e.clientX);
+});
---
+++ SCRIPT/projects.js
@@ -0,0 +1,97 @@
+const textContainer = document.querySelector(".text-container");
+const texts = document.querySelectorAll(".text");
+let currentIndex = 0;
+
+function toggleText() {
+  const currentText = texts[currentIndex];
+  const nextIndex = (currentIndex + 1) % texts.length;
+  const nextText = texts[nextIndex];
+
+  // Remove the flip class from the current text
+  currentText.classList.remove("flip");
+
+  // Add the flip class to the next text after a small delay to trigger the flip animation
+  setTimeout(() => {
+    nextText.classList.add("flip");
+  }, 10); // A small delay to ensure the flip animation applies
+
+  currentIndex = nextIndex;
+}
+
+// Toggle the text every 2 seconds (2000 milliseconds)
+setInterval(toggleText, 2000);
+
+let b = document.getElementById("footimg1");
+document.body.addEventListener("keydown", (press) => {
+  if (press.key == "Escape") {
+    window.location.href = "../index.html";
+  }
+});
+
+const container = document.getElementById("body");
+const divs = document.querySelectorAll(".a");
+
+let currentIndeX = 0;
+
+function scrollToIndex(index) {
+  if (index >= 0 && index < divs.length) {
+    divs[currentIndeX].classList.remove("highlighted");
+    currentIndeX = index;
+    divs[currentIndeX].classList.add("highlighted");
+    divs[currentIndeX].scrollIntoView({
+      behavior: "smooth",
+      block: "nearest",
+    });
+  }
+}
+
+// body.addEventListener("click",function()=> {
+
+// })
+
+container.addEventListener("keydown", (event) => {
+  if (event.key == "Enter" && currentIndeX == 0) {
+    window.open(
+      "https://abhinavpanwar.github.io/STONE_PAPER_SCISSORS/",
+      "_blank"
+    );
+  }
+  if (event.key == "Enter" && currentIndeX == 1) {
+    window.open("https://abhinavpanwar.github.io/2023_WC_SCHEDULE/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 2) {
+    window.open(
+      "https://abhinavpanwar.github.io/FOOD_ORDERING_WEBSITE/FOOD_ORDERING_WEBSITE",
+      "_blank"
+    );
+  }
+  if (event.key == "Enter" && currentIndeX == 3) {
+    window.open(
+      "https://abhinavpanwar.github.io/AMAZON_PRIME_CLONE/",
+      "_blank"
+    );
+  }
+  if (event.key == "Enter" && currentIndeX == 4) {
+    window.open("https://kingofcards.netlify.app/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 5) {
+    window.open("https://abhinavpanwar.github.io/world_clock/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 6) {
+    window.open("https://abhinavpanwar.github.io/calculator/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 7) {
+    window.open("https://abhinavpanwar.github.io/AOT_QUIZ/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 8) {
+    window.open("https://abhinavpanwar.github.io/BILL/", "_blank");
+  }
+  if (event.key === "ArrowUp") {
+    scrollToIndex(currentIndeX - 1);
+  } else if (event.key === "ArrowDown") {
+    scrollToIndex(currentIndeX + 1);
+  }
+});
+
+// Initial highlighting
+divs[currentIndeX].classList.add("highlighted");
