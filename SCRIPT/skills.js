c87f2f3b4192eac06dcbb74e732d47e45d85cdb6
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2025-04-17 16:04:17 +0530

    Update skills.js

--- SCRIPT/skills.js
+++ SCRIPT/skills.js
@@ -1,89 +1,16 @@
-let hamMenuIcon = document.getElementById("ham-menu");
-let navBar = document.getElementById("nav-bar");
-let navLinks = navBar.querySelectorAll("li");
-let hide = document.getElementById("box");
+// Mobile Menu Toggle
+const hamMenuIcon = document.getElementById("ham-menu");
+const navBar = document.getElementById("nav-bar");
+const navLinks = navBar.querySelectorAll("li");
 
 hamMenuIcon.addEventListener("click", () => {
   navBar.classList.toggle("active");
   hamMenuIcon.classList.toggle("fa-times");
-  hide.style.display = hide.style.display === "none" ? "flex" : "none";
-});
-navLinks.forEach((navLinks) => {
-  navLinks.addEventListener("click", () => {
-    navBar.classList.remove("active");
-    hamMenuIcon.classList.toggle("fa-times");
-  });
 });
 
-particlesJS("particles-container", {
-  particles: {
-    number: {
-      value: 50,
-      density: {
-        enable: true,
-        value_area: 800,
-      },
-    },
-    color: {
-      value: "#00FF00" /* Bright neon green color */,
-    },
-    shape: {
-      type: "circle",
-      stroke: {
-        width: 0,
-        color: "#000000",
-      },
-    },
-    opacity: {
-      value: 0.5,
-      random: false,
-      anim: {
-        enable: false,
-        speed: 1,
-        opacity_min: 0.1,
-        sync: false,
-      },
-    },
-    size: {
-      value: 5,
-      random: true,
-      anim: {
-        enable: false,
-        speed: 40,
-        size_min: 0.1,
-        sync: false,
-      },
-    },
-    line_linked: {
-      enable: true,
-      distance: 150,
-      color: "#00FF00" /* Bright neon green color */,
-      opacity: 0.4,
-      width: 1,
-    },
-    move: {
-      enable: true,
-      speed: 6,
-      direction: "none",
-      random: false,
-      straight: false,
-      out_mode: "out",
-    },
-  },
-  interactivity: {
-    detect_on: "canvas",
-    events: {
-      onhover: {
-        enable: true,
-        mode: "repulse",
-      },
-    },
-    modes: {
-      repulse: {
-        distance: 100,
-        duration: 0.4,
-      },
-    },
-  },
-  retina_detect: true,
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.remove("fa-times");
+  });
 });
