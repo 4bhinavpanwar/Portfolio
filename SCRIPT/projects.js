37976c90a63c76a077a9c9f58b6a4ab64972279b
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2025-04-18 00:29:56 +0530

    Update projects.js

--- SCRIPT/projects.js
+++ SCRIPT/projects.js
@@ -51,54 +51,30 @@ function scrollToIndex(index) {
 
 container.addEventListener("keydown", (event) => {
   if (event.key == "Enter" && currentIndeX == 0) {
-    window.open(
-      "https://abhinavpanwar.github.io/STONE_PAPER_SCISSORS/",
-      "_blank"
-    );
-  }
-  if (event.key == "Enter" && currentIndeX == 1) {
-    window.open("https://abhinavpanwar.github.io/2023_WC_SCHEDULE/", "_blank");
-  }
-  if (event.key == "Enter" && currentIndeX == 2) {
-    window.open(
-      "https://abhinavpanwar.github.io/FOOD_ORDERING_WEBSITE/FOOD_ORDERING_WEBSITE",
-      "_blank"
-    );
-  }
-  if (event.key == "Enter" && currentIndeX == 3) {
     window.open("https://abhinavpanwar.github.io/VLSI/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 4) {
+  if (event.key == "Enter" && currentIndeX == 1) {
     window.open(
       "https://abhinavpanwar.github.io/AMAZON_PRIME_CLONE/",
       "_blank"
     );
   }
-  if (event.key == "Enter" && currentIndeX == 5) {
+  if (event.key == "Enter" && currentIndeX == 2) {
     window.open("https://utkarshpanwar.netlify.app/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 6) {
+  if (event.key == "Enter" && currentIndeX == 3) {
     window.open("https://kingofcards.netlify.app/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 7) {
-    window.open("https://abhinavpanwar.github.io/world_clock/", "_blank");
-  }
-  if (event.key == "Enter" && currentIndeX == 8) {
-    window.open("https://abhinavpanwar.github.io/calculator/", "_blank");
-  }
-  if (event.key == "Enter" && currentIndeX == 9) {
+  if (event.key == "Enter" && currentIndeX == 4) {
     window.open("https://abhinavpanwar.github.io/AOT_QUIZ/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 10) {
+  if (event.key == "Enter" && currentIndeX == 5) {
     window.open("https://mytierlist.netlify.app/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 11) {
+  if (event.key == "Enter" && currentIndeX == 6) {
     window.open("https://abhinavpanwar.github.io/CRUD/", "_blank");
   }
-  if (event.key == "Enter" && currentIndeX == 12) {
-    window.open("https://abhinavpanwar.github.io/BILL/", "_blank");
-  }
-  if (event.key == "Enter" && currentIndeX == 13) {
+  if (event.key == "Enter" && currentIndeX == 7) {
     window.open("https://virtua1assistant.netlify.app/", "_blank");
   }
   if (event.key === "ArrowUp") {
