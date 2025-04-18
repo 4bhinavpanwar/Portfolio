e7cedd7a25028bf4e72114437b793f9be7dde2e1
Abhinav Panwar <95019283+ABHINAVpanwar@users.noreply.github.com>
2025-04-18 16:08:34 +0530

    Update projects.js

--- SCRIPT/projects.js
+++ SCRIPT/projects.js
@@ -66,15 +66,18 @@ container.addEventListener("keydown", (event) => {
     window.open("https://kingofcards.netlify.app/", "_blank");
   }
   if (event.key == "Enter" && currentIndeX == 4) {
-    window.open("https://abhinavpanwar.github.io/AOT_QUIZ/", "_blank");
+    window.open("https://tic-tac-toe-ursh.onrender.com/", "_blank");
   }
   if (event.key == "Enter" && currentIndeX == 5) {
-    window.open("https://mytierlist.netlify.app/", "_blank");
+    window.open("https://abhinavpanwar.github.io/AOT_QUIZ/", "_blank");
   }
   if (event.key == "Enter" && currentIndeX == 6) {
-    window.open("https://abhinavpanwar.github.io/CRUD/", "_blank");
+    window.open("https://mytierlist.netlify.app/", "_blank");
   }
   if (event.key == "Enter" && currentIndeX == 7) {
+    window.open("https://abhinavpanwar.github.io/CRUD/", "_blank");
+  }
+  if (event.key == "Enter" && currentIndeX == 8) {
     window.open("https://virtua1assistant.netlify.app/", "_blank");
   }
   if (event.key === "ArrowUp") {
